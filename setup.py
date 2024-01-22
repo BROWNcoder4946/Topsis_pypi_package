ae1f1becc262206236451eaa4479b0bca1b3f3a3
Aditya Vashishta <adityavashishta3911@gmail.com>
2024-01-22 14:50:15 +0530

    final package v:1.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="Topsis_Aditya_102103546",
-    version="1.0.0",
+    version="1.0.2",
     description="This Python script implements the TOPSIS (Technique for Order Preference by Similarity to Ideal Solution) algorithm.",
     long_description=README,
     long_description_content_type="text/markdown",
